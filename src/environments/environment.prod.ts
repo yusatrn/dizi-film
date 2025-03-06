5296999f197b1c8f43abb96769ec6a0d6014301c
yusatrn <yusa.turan@iski.gov.tr>
2025-03-06 13:06:43 +0300

    page

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.themoviedb.org/3',
+  apiKey: '0d16cbad341d3fdcd0495081914f961d', // Replace with your TMDB API key
+  imageBaseUrl: 'https://image.tmdb.org/t/p/'
+};
